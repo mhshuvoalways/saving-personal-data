6d436e89eaa826570e8fe5c200ae0d7bd963ccbc
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-08-27 19:48:25 +0600

    add app rootstate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux";
 import Routers from "./app/Routers";
 import Helmet from "./app/Helmet";
+import { RootState } from "./store";
 
 const App: React.FC = () => {
+  const count = useSelector((state: RootState) => state);
+  console.log(count);
   return (
     <div className="mt-[58.5px] mb-[52px]">
       <Routers />
